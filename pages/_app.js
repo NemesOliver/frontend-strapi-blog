ae2eab848cc1af1cfe3e56a74dd0453d252f2d9a
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2022-01-25 20:01:50 +0000

    Script test

--- pages/_app.js
+++ pages/_app.js
@@ -10,17 +10,17 @@ function MyApp({ Component, pageProps }) {
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
       />
-
-      <Script id="ga-analytics" strategy="lazyOnload">
-        {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', ${process.env.NEXT_PUBLIC_GA_ID}, {
-        page_path: window.location.pathname,
-      });
-  `}
-      </Script>
+      <Script
+        id="ga-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {window.dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', ${process.env.NEXT_PUBLIC_GA_ID})
+        `,
+        }}
+      />
 
       <Layout>
         <Component {...pageProps} />

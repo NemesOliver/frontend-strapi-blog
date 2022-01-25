b94c4497b2ca863aadbc3549a7ea72122ea0e7ab
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2022-01-25 19:24:54 +0000

    branch preview

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
+import Script from "next/script";
 import "../styles/globals.css";
 import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+
+      <Script id="ga-analytics" strategy="lazyOnload">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', ${process.env.NEXT_PUBLIC_GA_ID}, {
+        page_path: window.location.pathname,
+      });
+  `}
+      </Script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 

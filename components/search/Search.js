7a3043a8af5430403c76c30486f8843d195e385b
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2022-01-27 16:44:49 +0000

    fetch suggestions + debounce

--- components/search/Search.js
+++ components/search/Search.js
@@ -1,53 +1,73 @@
-/**
- * 1. Get user input through onKeyUp
- *
- * 2. Use ?filters[filed][filterOption]=searchQuery to search through availble articles through onChange
- *    Maybe only return id and title with query
- *
- *  a) Map through response and return an array of titles? or map directly through response
- *
- * 3. render a list of titles
- *
- * 4. on click router.push to article
- */
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-import React from "react";
-
-// Demo array
-const suggestions = [
-  {
-    id: 2,
-    title: "Why are Hobbits so small?",
-  },
-  {
-    id: 3,
-    title: "Can I live up to 150 years old?",
-  },
-  {
-    id: 6,
-    title: "Wireless charger killed my rat!",
-  },
-];
+import URL from "../../utils/strapi_connection";
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState(searchTerm);
+  const [suggestions, setSuggestions] = useState([]);
+
+  const router = useRouter();
+
+  // Set up debounce timer to throttle requests
+  useEffect(() => {
+    const debounceTimer = 400;
+
+    const timerId = setTimeout(() => {
+      setDebouncedQuery(searchTerm);
+    }, debounceTimer);
+
+    return () => clearTimeout(timerId);
+  }, [searchTerm]);
+
+  // Fetch and filter articles based on debounced search term
+  useEffect(() => {
+    const search = async () => {
+      try {
+        const res = await fetch(
+          `${URL}?filters[title][$containsi]=${debouncedQuery}`
+        );
+        const { data } = await res.json();
+
+        setSuggestions(data);
+      } catch (e) {
+        console.warn(e.message);
+      }
+    };
+    search();
+  }, [debouncedQuery]);
+
+  // Navigate to article and clear search term and debounced query forcing the options menu to hide
+  const forwardToArticle = (id) => {
+    router.push(`/blog/${id}`);
+    setSearchTerm("");
+    setDebouncedQuery("");
+  };
+
   return (
-    <div className="relative w-[300px]">
+    <div className="relative max-w-[300px]">
       <input
-        className="py-1.5 pl-3.5 pr-[100px] rounded-md border-2 border-green-700 focus:outline-none focus:border-green-500 transition-colors "
+        onChange={(e) => setSearchTerm(e.target.value)}
+        value={searchTerm}
+        className="py-1.5 pl-3.5 w-full rounded-md border-2 border-green-700 focus:outline-none focus:border-green-500 transition-colors"
         type="text"
         placeholder="Search..."
         name="autocomplete"
         id="autocomplete"
       />
       <ul className="absolute bg-blue-200 w-full  rounded-md ">
-        {suggestions.map(({ id, title }) => (
-          <li
-            key={id}
-            className="bg-white py-1.5 px-3.5 border-b text-[18px] hover:bg-green-100 cursor-pointer active:bg-green-300 transition-colors "
-          >
-            {title}
-          </li>
-        ))}
+        {debouncedQuery &&
+          suggestions.map(({ id, attributes: { title } }) => (
+            <li
+              aria-label="option"
+              key={id}
+              onClick={() => forwardToArticle(id)}
+              className="bg-white py-1.5 px-3.5 border-b text-[18px] rounded-md hover:bg-green-100 cursor-pointer active:bg-green-300 transition-colors "
+            >
+              {title}
+            </li>
+          ))}
       </ul>
     </div>
   );
